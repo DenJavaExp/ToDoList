09a93c8ca4afcd6276e914259082dafa39ad1ffc
DenJavaExp <marmyshev.denis@mail.ru>
2023-04-16 19:50:23 +0200

    Initial commit

---
+++ app/src/main/java/com/todolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.todolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var fab: FloatingActionButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        fab = findViewById(R.id.fab)
+        fab?.setOnClickListener {
+
+        }
+
+    }
+}
