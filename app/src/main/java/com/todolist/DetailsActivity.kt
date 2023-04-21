1c4e36321af83d6586527f2bd2906d1fd145199e
DenJavaExp <marmyshev.denis@mail.ru>
2023-04-21 20:28:34 +0200

    Добавленна локализация.

--- app/src/main/java/com/todolist/DetailsActivity.kt
+++ app/src/main/java/com/todolist/DetailsActivity.kt
@@ -3,13 +3,22 @@ package com.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
-        val text = intent?.getStringExtra("text")
-        val editText = findViewById<EditText>(R.id.edit_text_title)// приведение к типу в <> указал тип который искать в активити
-        editText.hint = text
+        val text = intent?.getStringExtra(TITLE_FOR_DETAILS)
+        val textField = findViewById<TextInputLayout>(R.id.textField)
+        textField.hint = text
+
+
     }
+
+    companion object{
+        const val TITLE_FOR_DETAILS = "titleForDetails"
+    }
+
 }
--- app/src/main/java/com/todolist/MainActivity.kt
+++ app/src/main/java/com/todolist/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.todolist.DetailsActivity.Companion.TITLE_FOR_DETAILS
 import com.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var fab: FloatingActionButton? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,9 @@ class MainActivity : AppCompatActivity() {
         fab = findViewById(R.id.fab)
         fab?.setOnClickListener {
             val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-            intent.putExtra("text", "Введите текст")
+            intent.putExtra(TITLE_FOR_DETAILS, getString(R.string.title_for_details))
             startActivity(intent)
         }
     }
 }
+
