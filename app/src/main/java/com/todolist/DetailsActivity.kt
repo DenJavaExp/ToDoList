c403f687ca4d91f15bea8ece8614b9ec85b570bd
DenJavaExp <marmyshev.denis@mail.ru>
2023-04-21 19:30:09 +0200

    Добавилено функционал передачи текста.

--- app/src/main/java/com/todolist/DetailsActivity.kt
+++ app/src/main/java/com/todolist/DetailsActivity.kt
@@ -2,10 +2,14 @@ package com.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        val text = intent?.getStringExtra("text")
+        val editText = findViewById<EditText>(R.id.edit_text_title)// приведение к типу в <> указал тип который искать в активити
+        editText.hint = text
     }
 }
--- app/src/main/java/com/todolist/MainActivity.kt
+++ app/src/main/java/com/todolist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -8,7 +9,6 @@ import com.todolist.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private var fab: FloatingActionButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +18,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         fab = findViewById(R.id.fab)
         fab?.setOnClickListener {
-
+            val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+            intent.putExtra("text", "Введите текст")
+            startActivity(intent)
         }
-
     }
 }
